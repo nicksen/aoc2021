09636d1f2ceeca23c7f6f87fbff78cd6becfe5c0
Nicklas Moberg <nicklasmoberg@gmail.com>
2021-12-09 22:25:55 +0100

    day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,49 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    input =
+      args
+      |> String.trim()
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    calc(input, 80)
   end
 
   def part2(args) do
+    input =
+      args
+      |> String.trim()
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    calc(input, 256)
+  end
+
+  defp calc(input, days) do
+    initial_state = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    state =
+      Enum.reduce(input, initial_state, fn fish, acc ->
+        put_elem(acc, fish, elem(acc, fish) + 1)
+      end)
+
+    run(state, days)
+  end
+
+  defp run(lifetime, 0) do
+    Tuple.sum(lifetime)
+  end
+
+  defp run(lifetime, days) do
+    new = elem(lifetime, 0)
+
+    lifetime =
+      for i <- 1..(tuple_size(lifetime) - 1), reduce: lifetime do
+        acc -> put_elem(acc, i - 1, elem(acc, i))
+      end
+
+    lifetime = put_elem(lifetime, 6, elem(lifetime, 6) + new)
+    lifetime = put_elem(lifetime, 8, new)
+    run(lifetime, days - 1)
   end
 end
