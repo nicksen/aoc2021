d4f01692defa4eb4868555723da213831b0660e5
Nicklas Moberg <nicklasmoberg@gmail.com>
2021-12-09 19:54:32 +0100

    day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,34 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    {x, y} =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+      |> Enum.reduce({0, 0}, fn {dx, dy}, {x, y} -> {x + dx, y + dy} end)
+
+    x * y
   end
 
   def part2(args) do
+    {x, y, _} =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+      |> Enum.reduce({0, 0, 0}, fn {dx, dy}, {x, y, a} ->
+        ady = dx * a
+        {x + dx, y + ady, a + dy}
+      end)
+
+    x * y
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split(" ", parts: 2)
+    |> case do
+      ["forward", distance] -> {String.to_integer(distance), 0}
+      ["down", distance] -> {0, String.to_integer(distance)}
+      ["up", distance] -> {0, String.to_integer(distance) * -1}
+    end
   end
 end
