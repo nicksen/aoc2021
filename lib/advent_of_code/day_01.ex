42d882fbe441ac94eeadccec0c1ed311da9221b4
Nicklas Moberg <nicklasmoberg@gmail.com>
2021-12-09 19:57:20 +0100

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,31 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce({-1, 0}, fn curr, {cnt, prev} ->
+      if curr > prev do
+        {cnt + 1, curr}
+      else
+        {cnt, curr}
+      end
+    end)
+    |> elem(0)
   end
 
   def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.reduce({-1, 0}, fn curr, {cnt, prev} ->
+      if curr > prev do
+        {cnt + 1, curr}
+      else
+        {cnt, curr}
+      end
+    end)
+    |> elem(0)
   end
 end
